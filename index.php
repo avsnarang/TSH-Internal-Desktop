9b57e2f393c0e7bfea46964cdad5e67006d872dd
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-11 11:39:44 +0530

    changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start([
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
     <meta name="google-signin-client_id" content="584175274872-hlf4ccq2q15ki7ua6h2sanijnind4lj9.apps.googleusercontent.com">
     <link rel="stylesheet" href="overflow.css">
     <link rel="stylesheet" href="styles.css">
@@ -36,20 +36,42 @@ session_start([
 <div class="bg-img">
     <div class="content">
         <header>Login With</header>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
-        <script>
-            function onSignIn(googleUser) {
-                var profile = googleUser.getBasicProfile();
-                var email;
-                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                console.log('Name: ' + profile.getName());
-                console.log('Image URL: ' + profile.getImageUrl());
-                console.log('Email: ' + profile.getEmail());
-                id = profile.getId();
-                email = profile.getEmail(); // This is null if the 'email' scope is not present.
-                window.location='redirect.php?email='+email+'&id='+id;
-            }
-        </script>
+        <center>
+            <script src="https://accounts.google.com/gsi/client" async defer></script>
+            <script>
+                function handleCredentialResponse(response) {
+                    console.log("Encoded JWT ID token: " + response.credential);
+                    console.log("ID: " + response.sub);
+                    console.log('Full Name: ' + response.name);
+                    console.log("Email: " + response.email);
+                    email = response.email;
+                    window.location.href = "https://tsh.edu.in/redirect.php?email=" + email;
+                }
+                window.onload = function () {
+                google.accounts.id.initialize({
+                    client_id: "584175274872-hlf4ccq2q15ki7ua6h2sanijnind4lj9.apps.googleusercontent.com",
+                    callback: handleCredentialResponse
+                });
+                }
+            </script>
+            <div id="g_id_onload"
+                data-client_id="584175274872-hlf4ccq2q15ki7ua6h2sanijnind4lj9.apps.googleusercontent.com"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="handleCredentialResponse"
+                data-nonce=""
+                data-auto_prompt="false">
+            </div>
+
+            <div class="g_id_signin"
+                data-type="standard"
+                data-shape="rectangular"
+                data-theme="outline"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left">
+            </div>
+        </center>
     </div>
 </div>
 </body>
