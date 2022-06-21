e1d721293e8df0cb5cad7da92eb3afb6885d6d46
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-21 23:48:18 +0530

    changes

---
+++ adminOffice/head/about-it-head.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <link rel="stylesheet" href="/nav_bar.css" type="text/css">
+     <link rel="stylesheet" href="/button_style.css" type="text/css">
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>About - IT Head</title>
+     <style>
+          .text-box {
+               margin: 50px;
+          }
+          .text-box p{
+               text-align: left;
+               font-size: 22px;
+          }
+          .text-box ul{
+               text-align: left;
+               font-size: 22px;
+          }
+          .text-box li{
+               text-align: left;
+               font-size: 22px;
+          }
+     </style>
+</head>
+<body>
+     <nav>
+          <a href="homepage.html">Home</a>
+          <a href="reports.html">Reports</a>
+          <a href="work.html">Work</a>
+          <a href="about.html">About</a>
+          <div class="animation start-home"></div>
+     </nav>
+     <h1>About TSH Internal</h1>
+          <div class="text-box">
+               <p>
+                    This website is created for the sole purpose of Reporting from HoD(s)/Coordinators, Xseed Leader, Teachers. This website can also be used to 
+                    provide information to the Students in the future. 
+                    These reports help the Management to study and understand of all the different things happening in and around the school, like: 
+               </p>
+               <ul>
+                    <li>Counselling of the Students</li>
+                    <li>Classroom Teaching style of the Teachers</li>
+                    <li>Work Done in a day by Administrators</li>
+                    <li>Parental Contact with the School Counsellor</li>
+                    <li>HoD(s) and Coordinators Analysis</li>
+               </ul>
+          </div>
+</body>
+</html>
