2f66c7b888bb1faeabc2a59bc22c876429464d6b
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-21 20:42:09 +0530

    changes

--- adminOffice/head/it_head.php
+++ adminOffice/head/it_head.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="/overflow.css">
     <link rel="stylesheet" href="/nav_bar.css">
     <link rel="stylesheet" href="/button_style.css">
-    <link rel="stylesheet" href="/homepage_styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,23 +28,27 @@
     <title>IT Head</title>
 </head>
 <body>
-<a href="/adminOffice/head/it_head.html">Home</a>
-<a href="/adminOffice/head/head_reports.html">Reports</a>
-<script>
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-            window.location.href = "https://www.tsh.edu.in";
-        });
-    }
-    function onLoad() {
-        gapi.load('auth2', function() {
-            gapi.auth2.init();
-        });
-    }
-</script>
-<a href="#" onclick="signOut();">Sign out</a>
+<div class="topnav">
+    <a href="/adminOffice/head/it_head.php">Home</a>
+    <a href="/adminOffice/head/head_reports.html">Reports</a>
+    <a href="#">Work</a>
+    <a href="#">About</a>
+    <script>
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+                window.location.href = "https://www.tsh.edu.in";
+            });
+        }
+        function onLoad() {
+            gapi.load('auth2', function() {
+                gapi.auth2.init();
+            });
+        }
+    </script>
+    <a href="#" onclick="signOut();" class="split">Sign out</a>
+</div>
 <script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
 </body>
 </html>
