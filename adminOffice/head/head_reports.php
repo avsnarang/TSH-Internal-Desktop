56ddb9a26b9716e102b2efcb2b45748424fc3768
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-22 23:56:49 +0530

    changes

--- adminOffice/head/head_reports.php
+++ adminOffice/head/head_reports.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php
-        require '/adminOffice/head/linkages/header-admin.php';
-    ?>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="584175274872-hlf4ccq2q15ki7ua6h2sanijnind4lj9.apps.googleusercontent.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="/overflow.css">
+    <link rel="stylesheet" href="/nav_bar.css">
+    <link rel="stylesheet" href="/button_style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="/img/favicon.png">
     <title>IT Head Reports</title>
 </head>
 <body>
