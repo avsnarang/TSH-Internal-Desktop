782ada05514d4770ef6f957b2ea0f5dffe2f3b91
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-23 16:40:16 +0530

    changes

--- adminOffice/head/reports/dep_class-observation-reports.php
+++ adminOffice/head/reports/dep_class-observation-reports.php
@@ -11,8 +11,8 @@
 include_once '../head/linkages/top-nav-adminHead.php';
 ?>
 <div class="buttons">
-  <button type="button" class="fill" onclick="window.location.href='../head/Observations_Forms/observe_forms.php'">Forms</button>
-  <button type="button" class="fill" onclick="window.location.href='../head/Observations_Excel/observe_excel.php'">Excel</button>
+  <button type="button" class="fill" onclick="window.location.href='../Observations_Forms/observe_forms.php'">Forms</button>
+  <button type="button" class="fill" onclick="window.location.href='../Observations_Excel/observe_excel.php'">Excel</button>
 </div>
 </body>
 </html>
