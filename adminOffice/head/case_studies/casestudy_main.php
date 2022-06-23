e6b812ef87e24508099878f5a430cc41ec8c1a9f
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-06-23 16:24:46 +0530

    changes

---
+++ adminOffice/head/case_studies/casestudy_main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include_once '../../head/linkages/header-admin.php';
+    ?>
+    <style>
+    h1{
+        font-family: "Times New Roman",serif;
+        text-align: center;
+    }
+    .test p{
+        font-family: "Calibri",sans-serif;
+        text-align: center;
+    }
+    </style>
+    <title>Case-Studies Homepage (IT Head)</title>
+</head>
+<body>
+<?php
+    include_once '../../head/linkages/top-nav-adminHead.php';
+?>
+<div class="buttons">
+     <button type="button" class="fill" onclick="window.open('https://docs.google.com/forms/d/1n_OKufM6vy5MC31wsXjWC-g5IAUtBAxZj9GpTSNsaZg/edit?usp=sharing','_blank')">Student Case Study - Report</button>
+     <button type="button" class="fill" onclick="window.open('https://docs.google.com/forms/d/1pkM7trhAEzynAFT2vqR5WV4iIyRI6jQ8zNEfHxt2DOg/edit?usp=sharing','_blank')">Class Case Study - Report</button>
+</div>
+</body>
+</html>
