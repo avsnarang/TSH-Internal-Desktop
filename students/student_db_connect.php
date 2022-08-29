1d38849f47d0c31a481009faad0af8ed4ad22799
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-08-29 20:38:18 +0530

    changes

--- students/student_db_connect.php
+++ students/student_db_connect.php
@@ -7,7 +7,7 @@ try
 {
     $con=new PDO("mysql:host=$servername;dbname=u950483018_auth_verify",$username,$password);
     $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo 'connected';
+    //echo 'connected';
 }
 catch(PDOException $e)
 {
