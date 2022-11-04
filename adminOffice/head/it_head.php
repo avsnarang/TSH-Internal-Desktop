0c3d4c73d204be9b6c2443c6bc3a5d8d970b524c
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-11-04 23:01:44 +0530

    changes

--- adminOffice/head/it_head.php
+++ adminOffice/head/it_head.php
@@ -61,12 +61,8 @@
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
                 console.log('User signed out.');
+                window.location.href = "https://tsh.edu.in";
             });
-            <?php
-                session_start();
-                session_destroy();
-                header("Location:https://tsh.edu.in");
-            ?>
         }
         function onLoad() {
             gapi.load('auth2', function() {
