88d6b259ffc4abafe7490441ec4eba16009dccf3
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-11-04 23:10:00 +0530

    changes

--- logout.php
+++ logout.php
@@ -10,13 +10,13 @@
 </head>
 <body>
 <script>
-    (function signOut() {
+    function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             console.log('User signed out.');
             window.location.href = "https://tsh.edu.in";
         });
-    })
+    }
     function onLoad() {
         gapi.load('auth2', function() {
             gapi.auth2.init();
