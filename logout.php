c01400cc63cc5c9dd265c310e9aae26d056b471e
Aditya Veer Singh Narang <avsnarang@gmail.com>
2022-11-04 23:06:13 +0530

    changes

--- logout.php
+++ logout.php
@@ -12,12 +12,12 @@
 </head>
 <body>
 <script>
-    function signOut() {
+    (function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             console.log('User signed out.');
         });
-    }
+    })
     function onLoad() {
         gapi.load('auth2', function() {
             gapi.auth2.init();
