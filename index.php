0d773ec7053f8b7bc8ac74c7b5b77c65fab5b4c8
Aditya Veer Singh Narang <avsnarang@gmail.com>
2023-04-03 10:46:34 +0530

    updates

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-session_start([
-    'cookie_lifetime' => 600,
-    'read_and_close'  => true,
-]);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +5,7 @@ session_start([
     <meta name="google-signin-client_id" content="584175274872-hlf4ccq2q15ki7ua6h2sanijnind4lj9.apps.googleusercontent.com">
     <link rel="stylesheet" href="overflow.css">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="button_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,25 +26,32 @@ session_start([
     </style>
 </head>
 <body>
-<img src="/Images/HomePage/School Building - 2-min.jpg" alt="School Building">
-<script src="/clear_cookies.js"></script>
-<div class="bg-img">
-    <div class="content">
-        <header>Login With</header>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
-        <script>
-            function onSignIn(googleUser) {
-                var profile = googleUser.getBasicProfile();
-                var email;
-                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                console.log('Name: ' + profile.getName());
-                console.log('Image URL: ' + profile.getImageUrl());
-                console.log('Email: ' + profile.getEmail());
-                id = profile.getId();
-                email = profile.getEmail(); // This is null if the 'email' scope is not present.
-                window.location='redirect.php?email='+email+'&id='+id;
-            }
-        </script>
+<script src="clear_cookies.js"></script>
+<div class="full_page">
+    <img src="Images/HomePage/School Building - 2-min.jpg" alt="School Building">
+    <div class="feedback_block">
+        <h1>Feedback Form</h1>
+        <button type="button" class="fill_red" onclick="window.open('https://forms.gle/NvQt5i9U3Vqp9V687','_blank')">Session by Sapna Punj</button>
+    </div>
+    <div class="bg-img">
+        <div class="content">
+            <header>Login With</header>
+            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            <script>
+                function onSignIn(googleUser) {
+                    var profile = googleUser.getBasicProfile();
+                    var email;
+                    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                    console.log('Name: ' + profile.getName());
+                    console.log('Image URL: ' + profile.getImageUrl());
+                    console.log('Email: ' + profile.getEmail());
+                    id = profile.getId();
+                    email = profile.getEmail(); // This is null if the 'email' scope is not present.
+                    window.location='redirect.php?email='+email+'&id='+id;
+                    googleUser.disconnect();
+                }
+            </script>
+        </div>
     </div>
 </div>
 </body>
